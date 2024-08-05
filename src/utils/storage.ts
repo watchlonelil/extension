16e6f177b97ac7fea9faed6f238fdc1d68a4ec94
lonelil <51315646+lonelil@users.noreply.github.com>
2024-08-05 16:08:31 +0800

    fix lint

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -3,10 +3,7 @@ import { useStorage } from '@plasmohq/storage/hook';
 
 import { makeUrlIntoDomain } from '~utils/domains';
 
-export const DEFAULT_DOMAIN_WHITELIST = [
-  'watch.lonelil.ru',
-  'mw.lonelil.ru'
-];
+export const DEFAULT_DOMAIN_WHITELIST = ['watch.lonelil.ru', 'mw.lonelil.ru'];
 
 export const storage = new Storage();
 
