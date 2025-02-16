e1348c0cf0d49523b34a685895f402fb55df24d8
lonelil <51315646+lonelil@users.noreply.github.com>
2025-02-16 16:13:29 +0800

    Update storage.ts

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -3,7 +3,7 @@ import { useStorage } from '@plasmohq/storage/hook';
 
 import { makeUrlIntoDomain } from '~utils/domains';
 
-export const DEFAULT_DOMAIN_WHITELIST = ['watch.lonelil.ru', 'mw.lonelil.ru'];
+export const DEFAULT_DOMAIN_WHITELIST = ['watch.lonelil.ru', 'mw.lonelil.ru', 'watch.lonelil.org'];
 
 export const storage = new Storage();
 
